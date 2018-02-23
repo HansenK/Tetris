9a399477b6ef4f0fcf9b96965ce64ed45666361d
Thiago Hansen <34888762+HansenK@users.noreply.github.com>
2018-02-23 11:13:07 -0300

    Mudanças nos nomes dos blocos

--- tetris2.js
+++ tetris2.js
@@ -1,11 +1,13 @@
 var cores = new Array ("red", "blue", "green", "yellow", "purple", "brown", "orange");
 var posicoes = new Array("0px","30px","60px","90px");
-var qnm = new Array("p","s","t","q");
 var quantb = 0, quantt=0;
-var tamy,tamx, posx=0;;
-var timer,teste=0;;
+var tamy,tamx, initx=0;
+var timer,teste=0, cont=0;
+var posy = [];
+var posx = [];
+var posy2, posx2, nomp;
 
-function criabloco(nome, cors) // cria 1 bloco de 30X30px (contando com a borda)
+function criabloco(nome, cors)
 {
     var bloco = document.createElement("div");
     bloco.setAttribute('id', nome);
@@ -18,26 +20,25 @@ function criabloco(nome, cors) // cria 1 bloco de 30X30px (contando com a borda)
     sqr.style.backgroundColor = cors;
 }
 
-function criapeca() // cria uma peça de tamanho aleátorio de até 4X4
+function criapeca()
 {
     var a=0,b=0;
     var cor = cores[Math.floor((Math.random() * 7) + 0)];
-    var name;
     tamy = (Math.floor((Math.random() * 4) + 1));
     tamx = (Math.floor((Math.random() * 4) + 1));
-    posx = (Math.floor((Math.random() * (330-(tamy*30))) + 0));
-    while(posx%30!=0)
+    initx = (Math.floor((Math.random() * (330-(tamy*30))) + 0));
+    while(initx%30!=0)
     {
-        posx = (Math.floor((Math.random() * (330-(tamy*30))) + 0));
+        initx = (Math.floor((Math.random() * (330-(tamy*30))) + 0));
     }
     for(let y=0;y<tamy;y++)
     {
         for(let x=0;x<tamx;x++)
         {
-            name = qnm[y] + quantb;
+            name = "b" + quantb;
             criabloco(name,cor);
             document.getElementById(name).style.left = posicoes[a];
-            document.getElementById(name).style.left = `${parseInt(document.getElementById(name).style.left) + posx}px`;
+            document.getElementById(name).style.left = `${parseInt(document.getElementById(name).style.left) + initx}px`;
             document.getElementById(name).style.top = posicoes[b];
             quantb++;
             a++;
@@ -52,15 +53,12 @@ function descer()
 {
     var nm;
     quantt = quantb - (tamx * tamy);
-    for(let y=0;y<tamy;y++)
+    for(let y=quantt;y<quantb;y++)
     {
-        for(let x=0;x<tamx;x++)
-        {
-            nm = qnm[y]+quantt;
-            py =`${(parseInt(document.getElementById(nm).style.top) + 30)}px`; // faz com que o objeto desça 30px (seu tamanho)
-            document.getElementById(nm).style.top = py;
-            quantt++;
-        }
+        nm = "b" + y;
+        py =`${(parseInt(document.getElementById(nm).style.top) + 30)}px`; // faz com que o objeto desça 30px (seu tamanho)
+        document.getElementById(nm).style.top = py;
+        quantt++;
     }
     teste++;
     if(teste==(20-tamy)){
@@ -69,5 +67,6 @@ function descer()
         teste=0;
     }
 }
+
 
 criapeca();
